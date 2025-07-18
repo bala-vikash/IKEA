3fa2769f781448e8a4b56ff1a5ab97244139916b
bala-vikash <balavikash6@gmail.com>
2025-07-18 09:29:33 +0530

    Documented Homepage.ts

--- pages/Homepage.ts
+++ pages/Homepage.ts
@@ -1,12 +1,14 @@
 import { Page, Locator, expect } from "@playwright/test";
 
 export class Homepage {
+    //Define types of each variables at top level
     page: Page;
     url: string;
     searchBar: Locator;
     gift : Locator;
     collections : string;
 
+    // Initiliasing variables with their locators
     constructor(page: Page) {
         this.page = page;
         this.url = "https://www.ikea.com/in/en/";
@@ -17,24 +19,30 @@ export class Homepage {
         this.collections = '//div[contains(@id,"hnf-carousel__tabs-navigation-products")]/div/a/span';
     }
 
+    // async methods define below  , these are invoked in spec/test file
+
+    // This methods redirects to the IKEA WebPage
     async navigate() {
         await this.page.goto(this.url);
         await this.page.waitForLoadState("networkidle");
         await expect(this.page).toHaveTitle("IKEA India-Affordable home furniture, designs & ideas - IKEA");
     }
-
+    
+    //This method searches for Bookshelves
     async searchBookShelves() {
         await this.searchBar.click();
         await this.searchBar.fill("BookShelves");
         await this.page.keyboard.press("Enter");
     }
 
+    //This method searches for StudyChair
     async searchStudyChair() {
         await this.searchBar.click();
         await this.searchBar.fill("Study Chair");
         await this.page.keyboard.press("Enter");
     }
 
+    //This method fetches all the active links and collections displayed on the webpage
     async getCollections() {
     const collectionLocator = this.page.locator(this.collections);
     const collectionsCount = await collectionLocator.count();
@@ -45,7 +53,7 @@ export class Homepage {
     }
     }
 
-
+    // Clicks on gift carousel
     async clickOnGift()
     {
         await this.gift.click();
